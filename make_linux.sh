fc1ce8b859671881e34f092e3ba32013416a9f32
Mickael CASTAGNIER <gullradriel@gmail.com>
2018-11-13 12:54:28 +0100

    Make linux script

---
+++ make_linux.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+HOSTLINUX="nagios@vlpsoout01-supsat10.si.cnaf.info"
+
+rsync -azuv --exclude-from 'exclude-list.txt' * $HOSTLINUX:~/Src/LIB/
+
+ssh $HOSTLINUX "cd ~/Src/LIB ; make clean && make && cd ~/Src/LIB/examples && make clean && make"
+
