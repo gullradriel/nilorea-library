3353541b8ce84ed89c5b3df6f9a9fe9534514755
Nagios opsiDev <CNAF-E-OPSSI@cafdoc.onmicrosoft.com>
2020-01-10 14:15:36 +0100

    Working around addr2line

--- src/n_signals.c
+++ src/n_signals.c
@@ -84,6 +84,7 @@ int addr2line(char const * const program_name, void const * const addr)
 	sprintf(addr2line_cmd,"atos -o %.256s %p", program_name, addr); 
 #elif defined __windows__
 	sprintf(addr2line_cmd,"addr2line -f -p -e %s %p", program_name, addr); 
+	n_log( LOG_DEBUG , "cmd: %s" , addr2line_cmd );
 #else
 	sprintf(addr2line_cmd,"addr2line -f -p -e %.256s %p", program_name, addr); 
 #endif
