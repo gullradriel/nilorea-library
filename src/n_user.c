dd3e9ac2c5bec81a2943fb6fed5b29e7e905207f
Mickaël Castagnier <gullradriel@gmail.com>
2020-05-12 17:18:51 +0200

    WIP N_USERLIST refactoring

--- src/n_user.c
+++ src/n_user.c
@@ -17,118 +17,179 @@
 #include "nilorea/n_user.h"
 
 
-int create_list_of_user( N_USER *list, int max )
+
+N_USERLIST *userlist_new( int max )
 {
+    N_USERLIST *ulist = NULL ;
+    Malloc( ulist , N_USERLIST , 1 );
+    __n_assert( ulist , return NULL );
 
-    int IT=0;
+    Malloc( ulist -> list, N_USER , max );
+    __n_assert( ulist -> list , Free( ulist ) ; return NULL );
 
-    Malloc( list -> list, N_USER_BOX, max );
-
-    if( !list -> list )
-        return FALSE;
-
-    list -> max = max ;
-    list -> highter = 0;
-
-    for( IT = 0; IT < max ; IT ++)
+    ulist -> max = max ;
+    ulist -> highter = 0;
+    for( int it = 0; it < max ; it ++ )
     {
-        list-> list[IT].state=0 ;
-        list-> list[IT].netw = NULL;
+        ulist -> list[ it ] . state = 0 ;
+        ulist -> list[ it ] . nb_rec_pos = 10 ;
+        ulist -> list[ it ] . only_last_pos = 1 ;
+        ulist -> list[ it ] . id = -1 ;
+        Malloc( ulist -> list[ it ] . last_positions , VECTOR3D , 10 );
+        ulist -> list[ it ] . netw_waitlist = new_generic_list( 0 );
+        memset( ulist -> list[ it ] . position , 0 , 3 * sizeof( double ) );
+        ulist -> list[ it ] . netw  = NULL ;
+        memset( ulist -> list[ it ] . name , 0 , 1024 );
+        ulist -> list[ it ] . netw  = NULL ;
     }
 
-    /*
-             *  CONFIGURATION
-             */
-    pthread_mutex_init( &list -> user_bolt, NULL );
+    init_lock( ulist -> user_rwbolt );
 
-    return TRUE;
-
+    return ulist ;
 }
 
 
 
-int add_user( N_USER *list )
+int userlist_set_position_behavior( N_USERLIST *ulist , int id , int nb_rec_pos , int only_last_pos )
+{
+    __n_assert( ulist , return FALSE );
+    __n_assert( nb_rec_pos < 1 , return FALSE );
+    __n_assert( only_last_pos < 0 || only_last_pos > 1 , return FALSE );
+
+    read_lock( ulist -> user_rwbolt );
+    __n_assert( ulist -> list[ id ] . state == 0 , return FALSE );
+    unlock( ulist -> user_rwbolt );
+
+    write_lock( ulist -> user_rwbolt );
+    ulist -> list[ id ] . nb_rec_pos = nb_rec_pos ;
+    ulist -> list[ id ] . only_last_pos = only_last_pos ;
+    Realloc(  ulist -> list[ id ] . last_positions , VECTOR3D , nb_rec_pos );
+    __n_assert( ulist -> list[ id ] . last_positions , unlock( ulist -> user_rwbolt ); return FALSE );
+    unlock( ulist -> user_rwbolt );
+
+    return TRUE ;
+}
+
+
+
+int userlist_add( N_USERLIST *ulist )
 {
 
     int it = -1;
-
-    pthread_mutex_lock( &list -> user_bolt );
-
+    write_lock( ulist -> user_rwbolt );
     do
     {
-
         it++;
-
     }
-    while( list -> list[ it ] . state != 0);
-
-    list -> list[ it ] . state = 1;
-
-    if( it > list -> highter ) list -> highter = it;
-
-    pthread_mutex_unlock( &list -> user_bolt );
+    while( ulist -> list[ it ] . state != 0);
+    ulist -> list[ it ] . state = 1;
+    if( it > ulist -> highter ) ulist -> highter = it;
+    unlock( ulist -> user_rwbolt );
 
     return it;
-
 }
 
 
 
-int rem_user( N_USER *list,int id )
+int userlist_rem( N_USERLIST *ulist , int id )
 {
-
-    pthread_mutex_lock( &list -> user_bolt );
-
-    if( list -> list[ id ] . state == 0 )
-        return FALSE;
-
-    if( list -> list[ id ] . netw )
-        netw_close( &list -> list[ id ] . netw );
-
-    list -> list[ id ] . state = 0;
-
-    if( id == list -> highter ) list -> highter --;
-
-    pthread_mutex_unlock( &list -> user_bolt );
-
-    return TRUE;
-
-}
-
-
-
-int add_msg_to_all( N_USER *list, N_STR *msg )
-{
-
-    int it;
-
-    pthread_mutex_lock( &list -> user_bolt );
-
-    for( it = 0 ; it < list -> max ; it ++ )
+    write_lock( ulist -> user_rwbolt );
+    if( ulist -> list[ id ] . state == 0 )
     {
-
-        if( list -> list[ it ] . state != 0 )
-            netw_add_msg( list -> list[ it ] . netw, msg );
+        unlock( ulist -> user_rwbolt );
+        return FALSE;
     }
-
-    pthread_mutex_unlock( &list -> user_bolt );
-
+    ulist -> list[ id ] . state = 0 ;
+    ulist -> list[ id ] . nb_rec_pos = 10 ;
+    ulist -> list[ id ] . only_last_pos = 1 ;
+    ulist -> list[ id ] . id = -1 ;
+    Realloc(  ulist -> list[ id ] . last_positions , VECTOR3D , 10 );
+    list_empty( ulist -> list[ id ] . netw_waitlist );
+    memset( ulist -> list[ id ] . position , 0 , 3 * sizeof( double ) );
+    ulist -> list[ id ] . netw  = NULL ;
+    memset( ulist -> list[ id ] . name , 0 , 1024 );
+    if( id == ulist -> highter && id != 0 )
+    {
+        int it = id - 1 ;
+        while( it >= 0 )
+        {
+            if( ulist -> list[ id ] . state == 1 )
+            {
+                ulist -> highter  = it ;
+                break ;
+            }
+            it -- ;
+        }
+    }
+    else
+    {
+        ulist -> highter = 0 ;
+    }
+    unlock( ulist -> user_rwbolt );
     return TRUE;
+}
 
+/* mode = USERLIST_ALL , USERLIST_ONE , USERLIST_ALL_EXCEPT */
+int userlist_add_msg_to_ex( N_USERLIST *ulist , N_STR *msg , int mode , int id )
+{
+    __n_assert( ulist , return FALSE );
+
+    switch( mode )
+    {
+        case( USERLIST_ALL ):
+        read_lock( ulist -> user_rwbolt );
+        for( int it = 0 ; it < ulist -> highter ; it ++ )
+        {
+            if( ulist -> list[ it ] . state != 0 )
+                netw_add_msg( ulist -> list[ it ] . netw, msg );
+        }
+        unlock( ulist -> user_rwbolt );
+        break;
+        case( USERLIST_ALL_EXCEPT ):
+        read_lock( ulist -> user_rwbolt );
+        for( int it = 0 ; it < ulist -> highter ; it ++ )
+        {
+            if( it == id )
+                continue ;
+            if( ulist -> list[ it ] . state != 0 )
+                netw_add_msg( ulist -> list[ it ] . netw, msg );
+        }
+        unlock( ulist -> user_rwbolt );
+        break;
+        default:
+         case( USERLIST_ONE ):
+         read_lock( ulist -> user_rwbolt );
+         netw_add_msg( ulist -> list[ id ] . netw, msg );
+         unlock( ulist -> user_rwbolt );
+         break;
+    }
+    return TRUE ;
 }
 
 
 
-int add_msg_to_user( N_USER *list, int id, N_STR *msg )
+int userlist_add_msg_to( N_USERLIST *ulist , N_STR *msg , int id )
 {
+    return userlist_add_msg_to_ex( ulist , msg , USERLIST_ONE , id );
+}
 
-    pthread_mutex_lock( &list -> user_bolt );
 
-    if( list -> list[ id ] . state != 0 )
-        netw_add_msg( list -> list[ id ] . netw, msg );
 
-    pthread_mutex_unlock( &list -> user_bolt );
+int userlist_add_msg_to_all( N_USERLIST *ulist , N_STR *msg )
+{
+    return userlist_add_msg_to_ex( ulist , msg , USERLIST_ALL , -1 );
+}
 
-    return TRUE;
 
+
+int userlist_add_msg_to_all_except( N_USERLIST *ulist , N_STR *msg , int id )
+{
+    return userlist_add_msg_to_ex( ulist , msg , USERLIST_ALL_EXCEPT , id );
+}
+
+
+int userlist_destroy( N_USERLIST **ulist )
+{
+    __n_assert( (*ulist) , return FALSE );
+    return FALSE ;
 }
