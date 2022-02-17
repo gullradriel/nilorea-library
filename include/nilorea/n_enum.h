1842d3bf084037b7c2e397a8ad3e54542ae643ae
Nagios opsiDev <CNAF-E-OPSSI@cafdoc.onmicrosoft.com>
2022-02-17 22:17:54 +0100

    Added indexed macro funcs

--- include/nilorea/n_enum.h
+++ include/nilorea/n_enum.h
@@ -1,6 +1,6 @@
 /**\file n_enum.h
- * Macro to build enums and their tostring counterparts, a reduced version of https://github.com/FredBienvenu/N_ENUM
- *\author Castagnier Mickael, based on original FredBienvenu N_ENUM entry
+ * Macro to build enums and their tostring counterparts, a reduced version of https://github.com/FredBienvenu/SmartEnum
+ *\author Castagnier Mickael, based on original FredBienvenu SmartEnum entry
  *\version 1.0
  *\date 28/09/2021
  */
@@ -20,27 +20,40 @@ extern "C" {
   @{
 */
 
-/*! helper to build an N_ENUM */
+/* MACROS FOR N_ENUMS MACRO ENTRIES (START) */
+
+	/*! helper to build an N_ENUM */
 #define N_ENUM_ENTRY(class, method) class##_##method
 
-/* MACROS FOR N_ENUMS MACRO ENTRIES (START) */
-/*! internal: case element */
+	/*!	\internal utils macro case value */
 #define __N_ENUM_MACRO_ENTRY_CASE(element_name) case element_name:
-/*! internal: error element */
+/*!	\internal utils macros string compare */
+#define __N_ENUM_MACRO_ENTRY_COMPARE(element_name) if(!strcmp(#element_name, str_value))
+/*!	\internal utils macros string ex compare */
+#define __N_ENUM_MACRO_ENTRY_COMPARE_EX(element_name) if(!strcmp(#element_name + index, str_value))
+/*!	\internal utils macros unknown value */
 #define __N_ENUM_MACRO_ENTRY_UNKNOWN_VALUE(enum_name) __##enum_name##_UNKNOWN_VALUE__
-/*! internal: macro to convert N_ENUMS entry to a real ENUM element */
+
+
+/*! \internal: macro to convert N_ENUMS entry to a real ENUM element */
 #define __N_ENUM_MACRO_ENTRY_TO_ENUM_ELEMENT(element_name, element_value)\
 	element_name = element_value,
-/*! internal: macro to test N_ENUMS entry */
+/*! \internal: macro to test N_ENUMS entry */
 #define __N_ENUM_MACRO_ENTRY_TO_ISVALID_CASE(element_name, element_value)\
 	__N_ENUM_MACRO_ENTRY_CASE(element_name) return true;
-/*! internal: macro to convert N_ENUMS entry to a string element */
+/*! \internal: macro to convert N_ENUMS entry to a string element */
 #define __N_ENUM_MACRO_ENTRY_TO_TOSTRING_CASE(element_name, element_value)\
 	__N_ENUM_MACRO_ENTRY_CASE(element_name) return #element_name;
+/*! \internal: macro to convert string to N_ENUMS value*/
+#define __N_ENUM_MACRO_ENTRY_TO_FROMSTRING_COMPARE(element_name, element_value)\
+	 __N_ENUM_MACRO_ENTRY_COMPARE(element_name) return element_name;
+/*! \internal: macro to convert string to N_ENUMS value*/
+#define __N_ENUM_MACRO_ENTRY_TO_FROMSTRING_COMPARE_EX(element_name, element_value)\
+	 __N_ENUM_MACRO_ENTRY_COMPARE_EX(element_name) return element_name;
 /* MACROS FOR N_ENUMS MACRO ENTRIES (END) */
 
 /* MACRO FOR ENUM DECLARATION (START) */
-/*! internal: macro for ENUM declaration */
+/*! \internal: macro for ENUM declaration */
 #define __N_ENUM_DECLARE_ENUM(MACRO_DEFINITION, enum_name)\
 	typedef enum enum_name\
 {\
@@ -51,18 +64,24 @@ enum_name;
 /* MACRO FOR ENUM DECLARATION (END) */
 
 /* MACRO FOR N_ENUMS FUNCTIONS * PREFIX : __N_ENUM_FUNCTION_ */
-/*! internal: create a getter function */
+/*! \internal: create a getter function */
 #define __N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION, enum_name) __N_ENUM_FUNCTION(enum_name); // MACRO for function declaration (just add ; after function)
-/*! internal: create a test function */
+/*! \internal: create a test function */
 #define __N_ENUM_FUNCTION_ISVALID(enum_name)\
 	bool N_ENUM_ENTRY(enum_name, isValid)(enum_name value)
-/*! internal: create a to string function */
+/*! \internal: create a to string function */
 #define __N_ENUM_FUNCTION_TOSTRING(enum_name)\
 	const char* N_ENUM_ENTRY(enum_name, toString)(enum_name value)
+/*! \internal: create a value from string function */
+#define __N_ENUM_FUNCTION_FROMSTRING(enum_name)\
+	enum_name N_ENUM_ENTRY(enum_name, fromString)(const char* str_value)
+/*! \internal: create a value from string function */
+#define __N_ENUM_FUNCTION_FROMSTRING_EX(enum_name)\
+	enum_name N_ENUM_ENTRY(enum_name, fromStringEx)(const char* str_value, int index)
 /* MACRO FOR N_ENUMS FUNCTIONS (END) */
 
 /* MACRO FOR N_ENUMS FUNCTIONS DEFINITIONS * PREFIX : __N_ENUM_DEFINE_FUNCTION_ */
-/*! internal: create getter functions is_valid */
+/*! \internal: create getter functions is_valid */
 #define __N_ENUM_DEFINE_FUNCTION_ISVALID(MACRO_DEFINITION, enum_name)\
 	__N_ENUM_FUNCTION_ISVALID(enum_name)\
 {\
@@ -72,7 +91,7 @@ enum_name;
 		default: return false;\
 	}\
 }
-/*! internal: create getter functions to_string */
+/*! \internal: create getter functions toString */
 #define __N_ENUM_DEFINE_FUNCTION_TOSTRING(MACRO_DEFINITION, enum_name)\
 	__N_ENUM_FUNCTION_TOSTRING(enum_name)\
 {\
@@ -82,6 +101,20 @@ enum_name;
 		default: return 0;\
 	}\
 }
+/*! \internal: create getter functions fromString */
+#define __N_ENUM_DEFINE_FUNCTION_FROMSTRING(MACRO_DEFINITION, enum_name)\
+	__N_ENUM_FUNCTION_FROMSTRING(enum_name)\
+{\
+	MACRO_DEFINITION(__N_ENUM_MACRO_ENTRY_TO_FROMSTRING_COMPARE)\
+	return __N_ENUM_MACRO_ENTRY_UNKNOWN_VALUE(enum_name);\
+}
+/*! \internal: create getter functions fromStringEx */
+#define __N_ENUM_DEFINE_FUNCTION_FROMSTRING_EX(MACRO_DEFINITION, enum_name)\
+	__N_ENUM_FUNCTION_FROMSTRING_EX(enum_name)\
+{\
+	MACRO_DEFINITION(__N_ENUM_MACRO_ENTRY_TO_FROMSTRING_COMPARE_EX)\
+	return __N_ENUM_MACRO_ENTRY_UNKNOWN_VALUE(enum_name);\
+}
 /* MACRO FOR N_ENUMS FUNCTIONS DEFINITIONS (END) */
 
 /* N_ENUM DECLARATION */
@@ -89,14 +122,18 @@ enum_name;
 #define N_ENUM_DECLARE(MACRO_DEFINITION, enum_name)\
 	__N_ENUM_DECLARE_ENUM(MACRO_DEFINITION, enum_name)\
 	__N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION_ISVALID, enum_name)\
-	__N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION_TOSTRING, enum_name)
+	__N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION_TOSTRING, enum_name)\
+	__N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION_FROMSTRING, enum_name)\
+	__N_ENUM_DECLARE_FUNCTION(__N_ENUM_FUNCTION_FROMSTRING_EX, enum_name)
 /* N_ENUM DECLARATION (END) */
 
 /* N_ENUM DEFINITION */
 /*! Macro to define an N_ENUM */
 #define N_ENUM_DEFINE(MACRO_DEFINITION, enum_name)\
 	__N_ENUM_DEFINE_FUNCTION_ISVALID(MACRO_DEFINITION, enum_name)\
-	__N_ENUM_DEFINE_FUNCTION_TOSTRING(MACRO_DEFINITION, enum_name)
+	__N_ENUM_DEFINE_FUNCTION_TOSTRING(MACRO_DEFINITION, enum_name)\
+	__N_ENUM_DEFINE_FUNCTION_FROMSTRING(MACRO_DEFINITION, enum_name)\
+	__N_ENUM_DEFINE_FUNCTION_FROMSTRING_EX(MACRO_DEFINITION, enum_name)
 /* N_ENUM DEFINITION (END) */
 
 /**
